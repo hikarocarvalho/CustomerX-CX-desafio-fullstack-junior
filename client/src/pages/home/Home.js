2f847239f22719a9618f2425bcddae63e6edd9cd
hikarocarvalho <hikarofcarvalho@gmail.com>
2022-09-23 04:21:23 -0300

    feat(Logout): create logout function

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,21 +1,32 @@
-import {useState, useEffect, createContext} from 'react';
+import { useState, useEffect, createContext } from "react";
+import { useNavigate } from "react-router-dom";
 import ContactList from "../../components/contactList/ContactList";
 import CustomerList from "../../components/customerList/CustomerList";
-import './Home.css';
+import "./Home.css";
 
 const ContactsContext = createContext(null);
 
 export default function Home() {
   const [idCustomer, setIdCustomer] = useState();
-  useEffect(()=>{});
- return (
-  <div className="home">
-    <ContactsContext.Provider value={{idCustomer, setIdCustomer}}>
-      <CustomerList />
-      <ContactList />
-    </ContactsContext.Provider>
-  </div>
- );
+
+  const navigate = useNavigate();
+
+  const logout = (event) => {
+    event.preventDefault();
+    localStorage.removeItem("auth");
+    navigate("/login");
+  };
+
+  useEffect(() => {});
+  return (
+    <div className="home">
+      <i className="bi bi-arrow-right-square-fill logout" onClick={logout}></i>
+      <ContactsContext.Provider value={{ idCustomer, setIdCustomer }}>
+        <CustomerList />
+        <ContactList />
+      </ContactsContext.Provider>
+    </div>
+  );
 }
 
-export {ContactsContext};
+export { ContactsContext };
